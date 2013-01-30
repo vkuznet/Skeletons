96d5bc70ce39fea6216910fb1f9236e3ef1a3a42
Valentin Kuznetsov <vkuznet@gmail.com>
2013-01-30 14:22:31 -0500

    Add unit tests

---
+++ tests/cms_t.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#pylint: disable-msg=c0301,c0103
+
+"""
+CMS module unit tests
+"""
+
+# system modules
+import os
+import sys
+import shutil
+import StringIO
+import unittest
+
+# package modules
+import Skeletons
+from Skeletons.cms import test_cms_environment, generate
+
+class testCMS(unittest.TestCase):
+    "A test class"
+    def setUp(self):
+        "set up"
+        # some stuff to init for this class
+        self.cdir = os.getcwd()
+        self.cmssw_base = '%s/test_tmp' % self.cdir
+        os.makedirs(os.path.join(self.cmssw_base, 'src/SubSystem/MyProd/src'))
+        os.makedirs(os.path.join(self.cmssw_base, 'src/SubSystem/MyProd/plugin'))
+
+    def tearDown(self):
+        "tear down"
+        os.chdir(self.cdir)
+        shutil.rmtree(self.cmssw_base)
+
+    def test_test_cms_environment(self):
+        "Test test_cms_environment function"
+        os.chdir(self.cdir)
+
+        expect = False
+        result = test_cms_environment('EDProducer')
+        self.assertEqual(expect, result)
+
+        os.environ['CMSSW_BASE'] = self.cmssw_base
+
+        os.chdir(os.path.join(self.cmssw_base, 'src/SubSystem'))
+        expect = 'subsystem'
+        result = test_cms_environment('EDProducer')
+        self.assertEqual(expect, result)
+
+        os.chdir(os.path.join(self.cmssw_base, 'src/SubSystem/MyProd'))
+        expect = False
+        result = test_cms_environment('EDProducer')
+        self.assertEqual(expect, result)
+
+        os.chdir(os.path.join(self.cmssw_base, 'src/SubSystem/MyProd/src'))
+        expect = 'src'
+        result = test_cms_environment('EDProducer')
+        self.assertEqual(expect, result)
+
+        os.chdir(os.path.join(self.cmssw_base, 'src/SubSystem/MyProd/plugin'))
+        expect = 'plugin'
+        result = test_cms_environment('EDProducer')
+        self.assertEqual(expect, result)
+
+    def test_mkedprod(self):
+        "Test creation of EDProducer code"
+        old_stdout = sys.stdout
+        sys.stdout = StringIO.StringIO()
+        os.chdir(self.cdir)
+        os.environ['CMSSW_BASE'] = self.cmssw_base
+        test_dir = os.path.join(self.cmssw_base, 'src/SubSystem')
+        os.chdir(test_dir)
+
+        sdir   = '/'.join(Skeletons.__file__.split('/')[:-1])
+        tdir   = '%s/scripts/mkTemplates' % '/'.join(sdir.split('/')[:-2])
+        kwds   = {'author': '', 'tmpl': 'EDProducer', 'pname':'MyPkg',
+                  'args': {}, 'debug': False, 'tmpl_dir': tdir}
+        generate(kwds)
+        dirs   = os.listdir(test_dir)
+        expect = True
+        result = 'MyPkg' in dirs
+        self.assertEqual(expect, result)
+
+        sys.stdout = old_stdout
+
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/utils_t.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#pylint: disable-msg=c0301,c0103
+
+"""
+Skeletons.utils unit tests
+"""
+
+# system modules
+import sys
+import unittest
+
+# package modules
+from Skeletons.utils import parse_word, functor, get_code_generator
+
+class testUtils(unittest.TestCase):
+    "A test class"
+    def setUp(self):
+        "set up"
+        # some stuff to init for this class
+        pass
+
+    def test_parse_word(self):
+        "Test parse_word function"
+        sts = "sdlkfj __class__::Bla"
+        result = parse_word(sts)
+        expect = set(["__class__"])
+        self.assertEqual(expect, result)
+
+    def test_functor(self):
+        "Test functor function"
+        snippet = """
+#python_begin
+    for i in __data__:
+        print i
+#python_end
+        """
+        data   = range(5)
+        result = functor(snippet, {'__data__':data})
+        expect = '\n'.join([str(i) for i in data]) + '\n'
+        self.assertEqual(expect, result)
+
+    def test_get_code_generator(self):
+        "Test get_code_generator function"
+        kwds   = {'author': '', 'tmpl': 'EDProducer', 'pname':'MyProd',
+                  'args': {}, 'debug': False, 'tmpl_dir': ''}
+        obj = get_code_generator(kwds)
+        result = str(type(obj))
+        expect = """<class 'Skeletons.pkg.AbstractPkg'>"""
+        self.assertEqual(expect, result)
+
+        kwds   = {'author': '', 'tmpl': 'TSelector', 'pname':'MyProd',
+                  'args': {}, 'debug': False, 'tmpl_dir': ''}
+        obj = get_code_generator(kwds)
+        result = str(type(obj))
+        expect = """<class 'Skeletons.tselector.TSelector'>"""
+        self.assertEqual(expect, result)
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
