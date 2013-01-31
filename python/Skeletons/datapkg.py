119e947ce0a3c835abc41dc1278340f372760476
Valentin Kuznetsov <vkuznet@gmail.com>
2013-01-31 11:47:50 -0500

    Set __pkgname__ to be used in templates

--- python/Skeletons/datapkg.py
+++ python/Skeletons/datapkg.py
@@ -7,7 +7,8 @@ File       : datapkg.py
 Author     : Valentin Kuznetsov <vkuznet@gmail.com>
 Description: generates DataPkg CMSSW infrastructure
 """
-
+# system modules
+import os
 # package modules
 from Skeletons.pkg import AbstractPkg
 
@@ -23,5 +24,8 @@ class DataPkg(AbstractPkg):
     def cpp_files(self, kwds=None):
         "Generate C++ files"
         kwds.update({'cpp_files':self.get_tmpl('.h') + self.get_tmpl('.xml')})
-        kwds.update({'cpp_dir': 'plugins'}) # location of cpp files
+        if  not kwds.has_key('cpp_dir'):
+            kwds.update({'cpp_dir': 'plugins'}) # location of cpp files
+        else: # call within SubSystem/Package/plugins area
+            kwds.update({'__pkgname__': os.getcwd().split('/')[-2]})
         return super(DataPkg, self).cpp_files(kwds)
