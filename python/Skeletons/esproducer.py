dfb01effadab0029f5e4ab9e8a2aaeb74c97aa8c
Valentin Kuznetsov <vkuznet@gmail.com>
2013-01-31 11:48:09 -0500

    Set __pkgname__ to be used in templates

--- python/Skeletons/esproducer.py
+++ python/Skeletons/esproducer.py
@@ -7,7 +7,8 @@ File       : esproducer.py
 Author     : Valentin Kuznetsov <vkuznet@gmail.com>
 Description: generates ESProducer CMSSW infrastructure
 """
-
+# system modules
+import os
 # package modules
 from Skeletons.pkg import AbstractPkg
 
@@ -26,5 +27,8 @@ class ESProducer(AbstractPkg):
         if  not args:
             args = {'__record__': 'MyRecord', '__datatypes__': ['MyData']}
         kwds.update(args)
-        kwds.update({'cpp_dir': 'plugins'}) # location for cpp files
+        if  not kwds.has_key('cpp_dir'):
+            kwds.update({'cpp_dir': 'plugins'}) # location for cpp files
+        else: # call within SubSystem/Package/plugins area
+            kwds.update({'__pkgname__': os.getcwd().split('/')[-2]})
         return super(ESProducer, self).cpp_files(kwds)
