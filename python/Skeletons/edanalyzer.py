d81551780e432e18af67917cb7aa4d2d3c920ddf
Valentin Kuznetsov <vkuznet@gmail.com>
2013-01-31 11:47:56 -0500

    Set __pkgname__ to be used in templates

--- python/Skeletons/edanalyzer.py
+++ python/Skeletons/edanalyzer.py
@@ -7,7 +7,8 @@ File       : edanalyzer.py
 Author     : Valentin Kuznetsov <vkuznet@gmail.com>
 Description: generates edanalyzer CMSSW infrastructure
 """
-
+# system modules
+import os
 # package modules
 from Skeletons.pkg import AbstractPkg
 
@@ -22,5 +23,8 @@ class EDAnalyzer(AbstractPkg):
         
     def cpp_files(self, kwds):
         "Generate C++ files"
-        kwds.update({'cpp_dir': 'plugins'}) # location for cpp files
+        if  not kwds.has_key('cpp_dir'):
+            kwds.update({'cpp_dir': 'plugins'}) # location for cpp files
+        else: # call within SubSystem/Package/plugins area
+            kwds.update({'__pkgname__': os.getcwd().split('/')[-2]})
         return super(EDAnalyzer, self).cpp_files(kwds)
