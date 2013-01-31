aabb49aa29925571d43fca436b92d3098bc8c0fd
Valentin Kuznetsov <vkuznet@gmail.com>
2013-01-31 11:48:04 -0500

    Set __pkgname__ to be used in templates

--- python/Skeletons/edproducer.py
+++ python/Skeletons/edproducer.py
@@ -7,7 +7,8 @@ File       : edproducer.py
 Author     : Valentin Kuznetsov <vkuznet@gmail.com>
 Description: generates edproducer CMSSW infrastructure
 """
-
+# system modules
+import os
 # package modules
 from Skeletons.pkg import AbstractPkg
 
@@ -22,5 +23,8 @@ class EDProducer(AbstractPkg):
         
     def cpp_files(self, kwds):
         "Generate C++ files"
-        kwds.update({'cpp_dir': 'plugins'}) # location for cpp files
+        if  not kwds.has_key('cpp_dir'):
+            kwds.update({'cpp_dir': 'plugins'}) # location for cpp files
+        else: # call within SubSystem/Package/plugins area
+            kwds.update({'__pkgname__': os.getcwd().split('/')[-2]})
         return super(EDProducer, self).cpp_files(kwds)
